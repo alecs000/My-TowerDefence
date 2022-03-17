b63b33e081f3f0c31401eb344ee2a89d8817e7a0
alecs000 <alecs.2004fed@gmail.com>
2022-03-17 12:51:58 +0300

    C33

--- EconomicTower/Assets/ForMyGame/2/Scripts/Click.cs
+++ EconomicTower/Assets/ForMyGame/2/Scripts/Click.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 using UnityEngine.EventSystems;
 
-public class Click : MonoBehaviour, IPointerClickHandler
+public class Click : MonoBehaviour
 {
     public GameObject up;
-    
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        up.gameObject.SetActive(true);
-    }
+
     public void Close(GameObject gm)
     {
         gm.SetActive(false);
+    }
+    void Update()
+    {
+        RaycastHit hit;
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                Debug.DrawRay(transform.position, Input.mousePosition, Color.red);
+                Debug.Log(hit.collider.gameObject.name);
+                if (hit.collider.gameObject.CompareTag("Upgrade"))
+                {
+                    
+                    up.gameObject.SetActive(true);
+                }
+            }
+        }
     }
 }
